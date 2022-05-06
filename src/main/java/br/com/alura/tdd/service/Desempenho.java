5dc102fcdeec956ec555b7112784aff8468a213a
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2022-05-05 22:04:04 -0300

    module 4

--- src/main/java/br/com/alura/tdd/service/Desempenho.java
+++ src/main/java/br/com/alura/tdd/service/Desempenho.java
@@ -5,29 +5,25 @@ import java.math.BigDecimal;
 public enum Desempenho {
     A_DESEJAR {
         @Override
-        public BigDecimal percentualReajuste() {
-            // TODO Auto-generated method stub
+        public BigDecimal percentualReajuste() {            
             return new BigDecimal("0.03");
         }
     },
     BOM {
         @Override
         public BigDecimal percentualReajuste() {
-            // TODO Auto-generated method stub
             return new BigDecimal("0.15");
         }
     },
     OTIMO {
         @Override
         public BigDecimal percentualReajuste() {
-            // TODO Auto-generated method stub
             return new BigDecimal("0.20");
         }
     },
     ESPETACULAR {
         @Override
         public BigDecimal percentualReajuste() {
-            // TODO Auto-generated method stub
             return new BigDecimal("0.40");
         }
 
--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -1,6 +1,7 @@
 package br.com.alura.tdd.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -10,12 +11,11 @@ import org.junit.jupiter.api.Test;
 import br.com.alura.tdd.modelo.Funcionario;
 
 public class BonusServiceTest {
-    
+
     @Test
     void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
         BonusService service = new BonusService();
-        BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
-        assertEquals(BigDecimal.ZERO, bonus);
+        assertThrows(IllegalArgumentException.class, () -> service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000"))));
     }
 
     @Test
@@ -32,3 +32,4 @@ public class BonusServiceTest {
         assertEquals(new BigDecimal("1000.0"), bonus);
     }
 }
+    
